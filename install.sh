84b2732a9df081b650baee32da8b24978c50595e
zrb <mrzhangrb@outlook.com>
2020-04-29 17:45:51 +0800

    <U+1F41B> FIX: something

--- install.sh
+++ install.sh
@@ -14,4 +14,13 @@ source ~/ownvimconfig/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
+git submodule init
+git submodule update
+
+cd ~/ownvimconfig/bundle/coc.nvim
+./install.sh
+
+cd ~/ownvimconfig/bundle/LeaderF
+./install.sh
+
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
